71784495e99f2a3804f3992885e99af3f91d8acc
ainaramaroto <ainara.maroto@opendeusto.es>
2025-03-30 20:59:25 +0200

    Clases AdministradorRepository y clienteRepository

--- src/main/java/com/example/restapi/repository/UsuarioRepository.java
+++ src/main/java/com/example/restapi/repository/UsuarioRepository.java
@@ -4,7 +4,7 @@ import com.example.restapi.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+public interface UsuarioRepository extends JpaRepository<Usuario, String> {
 	Optional<Usuario> findBynombreUsuario(String nombreUsuario);
     Optional<Usuario> findByEmail(String email);
     boolean existsByEmail(String email);
